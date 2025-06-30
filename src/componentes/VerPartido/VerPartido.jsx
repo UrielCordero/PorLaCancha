d6a6b27073100d2033c06585e6fedb58be03a4c8
Luckxyor <lucaseialben2@gmail.com>
2025-06-30 09:57:46 -0300

    Orden diseño cards ver partidos

--- src/componentes/VerPartido/VerPartido.jsx
+++ src/componentes/VerPartido/VerPartido.jsx
@@ -223,29 +223,31 @@ const VerPartido = () => {
         ) : partidosFiltrados.length === 0 ? (
           <p>No hay partidos disponibles con los filtros aplicados</p>
         ) : (
-          partidosFiltrados.map((partido) => (
-            <div key={partido.id_Partidos} className="partido-card">
-              <img
-                src={partido.Cancha?.FotoCancha || 'https://via.placeholder.com/300x150'}
-                alt="Foto cancha"
-                className="partido-imagen"
-              />
-              <div className="partido-info">
-                <p><strong>Hora:</strong> {partido.horaInicio} - {partido.horaFin}</p>
-                <p><strong>Cancha:</strong> {partido.Cancha?.nombre || 'Desconocida'}</p>
-                <p><strong>Precio:</strong> ${partido.Cancha?.precioXHora || 'Desconocido'}</p>
+          <div className="partidos-grid">
+            {partidosFiltrados.map((partido) => (
+              <div key={partido.id_Partidos} className="partido-card">
+                <img
+                  src={partido.Cancha?.FotoCancha || 'https://via.placeholder.com/300x150'}
+                  alt="Foto cancha"
+                  className="partido-imagen"
+                />
+                <div className="partido-info">
+                  <p><strong>Hora:</strong> {partido.horaInicio} - {partido.horaFin}</p>
+                  <p><strong>Cancha:</strong> {partido.Cancha?.nombre || 'Desconocida'}</p>
+                  <p><strong>Precio:</strong> ${partido.Cancha?.precioXHora || 'Desconocido'}</p>
 
-                <div className="boton-unirse-container">
-                  <button
-                    className="boton-crear boton-unirse"
-                    onClick={() => navigate(`/ver-info-partido/${partido.id_Partidos}`)}
-                  >
-                    Ver mas info
-                  </button>
+                  <div className="boton-unirse-container">
+                    <button
+                      className="boton-crear boton-unirse"
+                      onClick={() => navigate(`/ver-info-partido/${partido.id_Partidos}`)}
+                    >
+                      Ver mas info
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
+            ))}
+          </div>
         )}
       </div>
     </div>
