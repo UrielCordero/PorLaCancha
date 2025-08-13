deaf249d477698cb57009508561c0567314dc683
Android Studio <devandroid@ort>
2025-08-13 10:04:53 -0300

    Arreglando cositas y mejorando el sidebar

--- src/componentes/MenuLateral/MenuLateral.jsx
+++ src/componentes/MenuLateral/MenuLateral.jsx
@@ -185,37 +185,59 @@ function MenuLateral({ menuOpen, onClose, user, onLogout }) {
           <ul>
             <li>
               <Link to="/ver-partidos" onClick={onClose} className="menu-link">
-                <span className="menu-icon">⚽</span>
+                <span className="menu-icon">
+                  <i className="fas fa-futbol" style={{ fontSize: '20px' }}></i>
+                </span>
                 <span>Partidos</span>
               </Link>
             </li>
             <li>
               <Link to="/torneos" onClick={onClose} className="menu-link">
-                <span className="menu-icon">🏆</span>
+                <span className="menu-icon">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M19 5h-2V3H7v2H5C3.9 5 3 5.9 3 7v1c0 2.55 1.92 4.63 4.39 4.94.63 1.5 1.98 2.63 3.61 2.96V19H7v2h10v-2h-4v-3.1c1.63-.33 2.98-1.46 3.61-2.96C19.08 12.63 21 10.55 21 8V7c0-1.1-.9-2-2-2zM5 8V7h2v3.82C5.84 10.4 5 9.3 5 8zm14 0c0 1.3-.84 2.4-2 2.82V7h2v1z"/>
+                  </svg>
+                </span>
                 <span>Torneos</span>
               </Link>
             </li>
             <li>
               <Link to="/equipos" onClick={onClose} className="menu-link">
-                <span className="menu-icon">👥</span>
+                <span className="menu-icon">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
+                  </svg>
+                </span>
                 <span>Equipos</span>
               </Link>
             </li>
             <li>
               <Link to="/mis-partidos" onClick={onClose} className="menu-link">
-                <span className="menu-icon">📅</span>
+                <span className="menu-icon">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM7 10h5v5H7z"/>
+                  </svg>
+                </span>
                 <span>Mis Partidos</span>
               </Link>
             </li>
             <li>
               <Link to="/mi-equipo" onClick={onClose} className="menu-link">
-                <span className="menu-icon">🛡️</span>
+                <span className="menu-icon">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
+                  </svg>
+                </span>
                 <span>Mi Equipo</span>
               </Link>
             </li>
             <li>
               <Link to="/mis-torneos" onClick={onClose} className="menu-link">
-                <span className="menu-icon">🎯</span>
+                <span className="menu-icon">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M19 5h-2V3H7v2H5C3.9 5 3 5.9 3 7v1c0 2.55 1.92 4.63 4.39 4.94.63 1.5 1.98 2.63 3.61 2.96V19H7v2h10v-2h-4v-3.1c1.63-.33 2.98-1.46 3.61-2.96C19.08 12.63 21 10.55 21 8V7c0-1.1-.9-2-2-2zM5 8V7h2v3.82C5.84 10.4 5 9.3 5 8zm14 0c0 1.3-.84 2.4-2 2.82V7h2v1z"/>
+                  </svg>
+                </span>
                 <span>Mis Torneos</span>
               </Link>
             </li>
@@ -231,7 +253,11 @@ function MenuLateral({ menuOpen, onClose, user, onLogout }) {
               onClose(); 
             }}
           >
-            <span className="logout-icon">🚪</span>
+            <span className="logout-icon">
+              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"/>
+              </svg>
+            </span>
             <span>Cerrar Sesión</span>
           </button>
         </div>
